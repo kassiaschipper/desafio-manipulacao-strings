73532d57027abc42891999791e2fffc94589f8b0
Kássia <kassia_guterres@outlook.com>
2023-07-08 00:32:36 -0300

    Q2- Remove duplicates function implemented

--- src/index.js
+++ src/index.js
@@ -7,4 +7,19 @@ function invertSentenceOrder(str) {
   return finalSentence;
 }
 
+function removeDuplicates(str) {
+  let result = "";
+
+  for(let i = 0; i<str.length; i++) {
+   
+    if(new String(str[i]).toLowerCase() !== new String(str[i+1]).toLowerCase()){
+      result += str[i]
+    }
+  
+  }
+  console.log(result)
+  return result;
+}
+
+removeDuplicates("Hello, Word!");
 invertSentenceOrder("Hello, Word! OpenAi is amazing.");
